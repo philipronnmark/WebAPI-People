bdf14302aca7f09a43096ec07741a59e6df17cba
mg19 <apperxd@gmail.com>
2021-07-28 03:49:57 +0200

    Added API methods

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,58 @@
+﻿using DemoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoAPI.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey", Id = 1 });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", Id = 2 });
+            people.Add(new Person { FirstName = "Philip", LastName = "Rönnmark", Id = 3 });
+
+        }
+        [Route("api/People/GetFirstNames/{userId:int}/{age:int}")]
+        [HttpGet]
+        public List<string> GetFirstNames(int userId, int age)
+        {
+            List<string> output = new List<string>();
+
+            foreach (var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+            return output;
+        }
+        // GET: api/People
+        public List<Person> Get()
+        { 
+            return people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/People
+        public void Post(Person val)
+        {
+            people.Add(val);
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+            people.Where(x => x.Id == id).FirstOrDefault();
+        }
+    }
+}
